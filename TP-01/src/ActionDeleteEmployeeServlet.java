c1e3beffa22d0d902cbf68bf777fe078e64d5823
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-04-19 23:26:46 -0300

    adicionando Servlet DeleteEmployee

--- TP-01/src/ActionDeleteEmployeeServlet.java
+++ TP-01/src/ActionDeleteEmployeeServlet.java
@@ -1,34 +1,49 @@
-
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import com.google.gson.Gson;
 
 import DAO.EmployeeDAO;
 import Entities.EmployeeEntity;
+import Entities.ResponseResult;
 
 public class ActionDeleteEmployeeServlet extends HttpServlet {
-  private static final long serialVersionUID = 1L;
+  private Gson _jsonHandler;
 
   public ActionDeleteEmployeeServlet() {
     super();
+    this._jsonHandler = new Gson();
   }
 
-  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    Integer id = Integer.parseInt(request.getParameter("id"));
+  public void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    response.setContentType("application/json");
+    response.setCharacterEncoding("UTF-8");
 
-    EmployeeEntity em = EmployeeDAO.getEmployeeById(id);
-    if (em != null) {
-      EmployeeDAO.delete(em.getId());
-      PrintWriter out = response.getWriter();
-      out.print("<body><script type=\"text/javascript\">\r\n" + "  	function redirect(){  		\r\n"
-          + "  		alert(\"employee successfully removed!\");\r\n"
-          + "  		window.location.replace(\"http://localhost:8080/TP-01/ReadServlet\");\r\n" + "  		\r\n"
-          + "  	}\r\n" + "  	redirect()\r\n" + "  </script></body>");
+    ResponseResult result;
+
+    try {
+      String message;
+
+      Integer employeeId = Integer.parseInt(request.getParameter("id"));
+
+      EmployeeEntity employee = EmployeeDAO.getEmployeeById(employeeId);
+      boolean employeeExists = employee != null && employee.getId() != 0;
+
+      if (!employeeExists) {
+        message = "Employee not found.";
+      } else {
+        EmployeeDAO.delete(employee.getId());
+
+        message = "Employee successfully deleted.";
+      }
+
+      result = new ResponseResult(employeeExists, message, null);
+    } catch (Exception exception) {
+      result = new ResponseResult(false, exception.getMessage(), exception);
     }
-    response.getWriter().append("Served at: " + id + " aa ").append(request.getContextPath());
-  }
 
-  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    response.getWriter().append("Served at:  POST ").append(request.getContextPath());
+    PrintWriter printWritter = response.getWriter();
+    printWritter.write(this._jsonHandler.toJson(result));
+    printWritter.flush();
   }
 }
